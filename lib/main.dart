595ac547a84e0186901ce7072b18e95c21f2783c
sanchezJose07 <l18141223@queretaro.tecnm.mx>
2024-09-18 20:51:15 -0600

    firstcommit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WidgetApp());
+}
+
+class WidgetApp extends StatelessWidget {
+  const WidgetApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AppBar(
+        title: const Text( "FLUTTER APP"),
+      ),
+    );
+  }
+}
